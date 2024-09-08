452592acf0589aa4aac9c20e1d79c73d0baff125
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-08 18:07:59 +0700

    feat: change tabs to flexible tabs count

---
+++ components/post/post-tabs.tsx
@@ -0,0 +1,35 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
+
+interface Tab {
+    trigger: string;
+    content: React.ReactNode;
+    value: string;
+  }
+  
+  interface TabsProps {
+    tabs: Tab[];
+    defaultValue?: string;
+  }
+  
+  export const TabsAll = ({ tabs, defaultValue }: TabsProps) => {
+    return (
+      <Tabs defaultValue={defaultValue || tabs[0].value} className="w-full">
+        <TabsList className="w-full h-[50px]">
+          {tabs.map((tab) => (
+            <TabsTrigger
+              key={tab.value}
+              className="font-bold text-lg w-full h-full data-[state=active]:border-b-[6px] data-[state=active]:border-blue-500"
+              value={tab.value}
+            >
+              {tab.trigger}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {tabs.map((tab) => (
+          <TabsContent key={tab.value} value={tab.value}>
+            {tab.content}
+          </TabsContent>
+        ))}
+      </Tabs>
+    );
+  };
