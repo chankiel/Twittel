b783a15a7e7777a1db74a61121e0507c5a028423
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-11 00:12:29 +0700

    feat: add like and bookmark func

---
+++ components/post/post-action.tsx
@@ -0,0 +1,30 @@
+import {toggleLike, toggleBookmark} from "@/lib/actions"
+
+interface PostActionProps{
+    type: string;
+    user_id: number;
+    post_id: number;
+    children: React.ReactNode;
+}
+
+export default function PostAction({
+    type,
+    user_id,
+    post_id,
+    children,
+}:PostActionProps){
+    let postAction = toggleLike;
+    if(type==="bookmark"){
+        postAction = toggleBookmark;
+    }
+
+    const postActionWithId = postAction.bind(null,user_id,post_id);
+
+    return (
+        <form className="w-[24px] h-full" action={postActionWithId}>
+            <button type="submit" className="w-full h-full">
+                {children}
+            </button>
+        </form>
+    )
+}
