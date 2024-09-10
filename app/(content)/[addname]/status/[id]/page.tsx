f65eb8cbdf07350b0a3d481dadaa0d07c1e072b9
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-11 00:13:02 +0700

    feat: apply fetch to post status

--- app/(content)/[addname]/status/[id]/page.tsx
+++ app/(content)/[addname]/status/[id]/page.tsx
@@ -2,8 +2,27 @@ import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { PostStatusCard } from "@/components/post/post-card";
 import { PostCards } from "@/components/post/post-cards";
+import { fetchPost } from "@/lib/actions";
+import { notFound } from "next/navigation";
 
-export default function Post() {
+export default async function Post({
+  params,
+}: {
+  params: { id: string; addname: string };
+}) {
+  const postData = await fetchPost({
+    where: {
+      author: {
+        addname: params.addname,
+      },
+      id: Number(params.id),
+    },
+  });
+
+  if (!postData) {
+    notFound();
+  }
+
   return (
     <div>
       <div className="flex items-center px-3 gap-7 py-4">
@@ -11,10 +30,11 @@ export default function Post() {
           <ArrowLeftIcon className="h-full" />
         </Link>
         <div>
-        <h1 className="text-2xl font-bold">Post</h1>        </div>
+          <h1 className="text-2xl font-bold">Post</h1>{" "}
+        </div>
       </div>
-      <PostStatusCard/>
-      <PostCards uploadAble={true} placeholder="Post your reply"/>
+      <PostStatusCard post={postData} />
+      <PostCards uploadAble={true} placeholder="Post your reply" />
     </div>
   );
 }
