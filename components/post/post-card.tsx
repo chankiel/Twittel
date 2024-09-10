2b109ee25e2ac0bdde927b67194e65764a525e64
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-10 20:12:24 +0700

    feat: connect db to post card

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -1,4 +1,3 @@
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   ChatBubbleLeftIcon,
@@ -8,45 +7,68 @@ import {
 } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { PostOptions } from "./post-option";
+interface PostCardProps {
+  id: number;
+  content: string;
+  datetime_post: Date;
+  author: {
+    username: string;
+    addname: string;
+  };
+  _count: {
+    likedBy: number;
+    bookmarkedBy: number;
+    replies: number;
+  };
+}
 
-interface PostCard {}
+export function PostCard({ post }: { post: PostCardProps }) {
+  const now = new Date();
+  let timeType = "s";
+  let diffTime = Math.floor((now.getTime() - post.datetime_post.getTime()) / 1000);
+  if (diffTime > 3600) {
+    diffTime = Math.floor(diffTime / 3600);
+    timeType = "j";
+  } else if (diffTime > 60) {
+    diffTime = Math.floor(diffTime / 60);
+    timeType = "m";
+  } else if (diffTime==0){
+    timeType = "now";
+  }
 
-export function PostCard() {
   return (
     <div className="flex p-3 border-b relative">
-      <PostOptions className="absolute right-4" />
+      <PostOptions className="absolute right-4" post_id={post.id} />
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
       <div className="ml-3 w-full">
         <h3 className="font-bold text-lg">
-          Faze Jason <span>@jasontheween . 3j</span>
+          {post.author.username}{" "}
+          <span>
+            @{post.author.addname} . {timeType==="now"?"":diffTime}{timeType}
+          </span>
         </h3>
-        <p className="mb-2">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
-          placeat totam maxime cum! Aliquam blanditiis atque vel similique
-          soluta quas molestiae quia fugit dolorem sit rerum voluptate, rem
-          molestias aut!
-        </p>
-        <Image
+        <p className="mb-2">{post.content}</p>
+        {/* <Image
           src={"/imagepost-1.png"}
           width={300}
           height={300}
           alt="image-post"
-        />
+        /> */}
         <div className="flex h-6 mt-4">
           <div className="w-full h-full flex items-center gap-[2px]">
             <ChatBubbleLeftIcon className="max-h-full" />
-            <p>41</p>
+            <p>{post._count.replies}</p>
           </div>
           <div className="w-full h-full flex items-center gap-[2px]">
             <HeartIcon className="max-h-full" />
-            <p>2 rb</p>
+            <p>{post._count.likedBy}</p>
           </div>
           <div className="w-full h-full flex items-center gap-[2px]">
             <Image src={"/retweet.png"} width={24} height={24} alt="retweet" />
-            <p>2 rb</p>
+            <p>{post._count.bookmarkedBy}</p>
           </div>
           <div className="w-full h-full flex  items-center justify-end gap-1">
             <BookmarkIcon className="max-h-full" />
@@ -58,28 +80,31 @@ export function PostCard() {
   );
 }
 
-export function PostStatusCard() {
+export function PostStatusCard({
+  post,
+  datetime_post,
+}: {
+  post: PostCardProps;
+  datetime_post: Date;
+}) {
   return (
     <>
       <div className="flex p-3 relative">
-        <PostOptions className="absolute right-4" />
+        <PostOptions className="absolute right-4" post_id={post.id} />
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
         <div className="ml-3 w-full">
-          <h3 className="font-bold text-lg leading-none">Faze Jason</h3>
-          <p>@jasontheween</p>
+          <h3 className="font-bold text-lg leading-none">
+            {post.author.username}
+          </h3>
+          <p>@{post.author.addname}</p>
         </div>
       </div>
       <div className="px-4">
-        <p className="mb-2 font-medium">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
-          placeat totam maxime cum! Aliquam blanditiis atque vel similique soluta
-          quas molestiae quia fugit dolorem sit rerum voluptate, rem molestias
-          aut!
-        </p>
-        <div className="flex justify-center">
+        <p className="mb-2 font-medium">{post.content}</p>
+        {/* <div className="flex justify-center">
           <Image
             src={"/imagepost-1.png"}
             width={300}
@@ -87,17 +112,19 @@ export function PostStatusCard() {
             alt="image-post"
             className="w-[90%]"
           />
-          </div>
-        <p className="my-3">4:59 AM . Sep 8, 2024 . <span className="font-bold">1,8M</span> Views</p>
+        </div> */}
+        <p className="my-3">
+          4:59 AM . Sep 8, 2024 . <span className="font-bold">1,8M</span> Views
+        </p>
         <hr />
         <div className="flex h-6 my-4">
           <div className="w-full h-full flex items-center gap-[2px]">
             <ChatBubbleLeftIcon className="max-h-full" />
-            <p>41</p>
+            <p>{post._count.replies}</p>
           </div>
           <div className="w-full h-full flex items-center gap-[2px]">
             <HeartIcon className="max-h-full" />
-            <p>2 rb</p>
+            <p>{post._count.likedBy}</p>
           </div>
           <div className="w-full h-full flex items-center gap-[2px]">
             <Image src={"/retweet.png"} width={24} height={24} alt="retweet" />
@@ -105,14 +132,14 @@ export function PostStatusCard() {
           </div>
           <div className="w-full h-full flex items-center gap-[2px]">
             <BookmarkIcon className="max-h-full" />
-            <p>2 rb</p>
+            <p>{post._count.bookmarkedBy}</p>
           </div>
           <div className="w-full h-full flex justify-end">
             <ShareIcon className="max-h-full" />
           </div>
         </div>
         <hr />
-        </div>
+      </div>
     </>
   );
 }
--- components/post/post-cards.tsx
+++ components/post/post-cards.tsx
@@ -1,19 +1,47 @@
-import { PostData } from "@/lib/placeholder-data";
 import { PostCard } from "./post-card";
 import { PostInput } from "./post-input";
+import prisma from "@/lib/db";
 
-interface PostCardsProps{
-    uploadAble: boolean;
-    placeholder?: string;
+interface PostCardsProps {
+  uploadAble: boolean;
+  placeholder?: string;
 }
 
-export function PostCards({uploadAble, placeholder="What's happening?!"}:PostCardsProps){
-    return(
-        <div>
-            {uploadAble && <PostInput placeholder={placeholder}/>}
-            {PostData.map((data)=>{
-                return <PostCard key={data.author}/>
-            })}
-        </div>
-    )
+export async function PostCards({
+  uploadAble,
+  placeholder = "What's happening?!",
+}: PostCardsProps) {
+  const cards = await prisma.post.findMany({
+    orderBy: {
+      datetime_post: "desc",
+    },
+    select: {
+      id: true,
+      content: true,
+      datetime_post:true,
+      author: {
+        select: {
+          username: true,
+          addname: true,
+        },
+      },
+      _count: {
+        select: { likedBy: true, bookmarkedBy: true, replies: true },
+      },
+    },
+  });
+
+  return (
+    <div>
+      {uploadAble && <PostInput placeholder={placeholder} />}
+      {cards.map((card) => {
+        return (
+          <PostCard
+            key={card.id}
+            post={card}
+          />
+        );
+      })}
+    </div>
+  );
 }
