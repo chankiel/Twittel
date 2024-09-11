624a21ad3734210628bc5772f1edac2615c31862
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-11 23:54:49 +0700

    refactor; add difftime formatter to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function diffNow(datetime_post:Date){
+  const now = new Date();
+  let timeType = "s";
+  let diffTime = Math.floor(
+    (now.getTime() - datetime_post.getTime()) / 1000
+  );
+  if (diffTime > 3600) {
+    diffTime = Math.floor(diffTime / 3600);
+    timeType = "j";
+  } else if (diffTime > 60) {
+    diffTime = Math.floor(diffTime / 60);
+    timeType = "m";
+  } else if (diffTime == 0) {
+    timeType = "now";
+  }
+  const diffString = (timeType==="now" ? timeType:diffTime+timeType) 
+  return diffString;
+}
