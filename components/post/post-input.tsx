8244a00cc81d93ca5d2fc1d27f00e1ad5d6e75e9
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-17 13:06:54 +0700

    feat: add auth post-input

--- components/post/post-input.tsx
+++ components/post/post-input.tsx
@@ -1,40 +1,94 @@
-'use client';
+"use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Textarea } from "../ui/textarea";
 import { createPost } from "@/lib/actions";
 import { useState } from "react";
-import { FormFooter } from "./form-footer";
+import { GifIcon, PhotoIcon } from "@heroicons/react/24/outline";
+import { Button } from "../ui/button";
+import { DialogClose } from "../ui/dialog";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import UserAvatar from "../parts/user-avatar";
 
-export function PostInput({ placeholder, parent_id = -1,isModal = false }: { placeholder?: string; parent_id?: number, isModal?:boolean }) {
+export function PostInput({
+  placeholder,
+  parent_id = -1,
+  isModal = false,
+}: {
+  placeholder?: string;
+  parent_id?: number;
+  isModal?: boolean;
+}) {
   const [content, setContent] = useState("");
+  const {data:session,status} = useSession();
   const isReply = parent_id !== -1;
   const isReplyModal = isReply && isModal;
+  const userId = session?.user?.id || '';
+  const {replace} = useRouter();
+
+  const buttonContent = (
+    <Button
+      className="ml-auto rounded-full text-lg font-bold"
+      type="submit"
+      disabled={content === ""}
+    >
+      {" "}
+      Post
+    </Button>
+  );
+
+  const insideForm = (
+    <>
+      <UserAvatar src={session?.user?.image}></UserAvatar>
+      <div className="w-full">
+        <Textarea
+          placeholder={placeholder}
+          className="text-xl"
+          name="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <div className="flex items-center mt-3 gap-3">
+          <PhotoIcon className="h-[25px]" />
+          <GifIcon className="h-[25px]" />
+          {isReplyModal ? (
+            <DialogClose asChild>{buttonContent}</DialogClose>
+          ) : (
+            <>{buttonContent}</>
+          )}
+        </div>
+      </div>
+    </>
+  );
+
+  if(status === "loading"){
+    return(
+      <div className="flex p-4 gap-2 border-b-2">
+        {insideForm}
+      </div>
+    )
+  }
+
+  if(status === "unauthenticated"){
+    replace("/");
+  }
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    
+
     if (isReply) {
-      await createPost(formData, parent_id);
+      await createPost(formData, userId, parent_id);
     } else {
-      await createPost(formData);
+      await createPost(formData,userId);
     }
-    
+
     setContent("");
   };
 
   return (
     <form className="flex p-4 gap-2 border-b-2" onSubmit={handleSubmit}>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <div className="w-full">
-        <Textarea placeholder={placeholder} className="text-xl" name="content" value={content} onChange={(e) => setContent(e.target.value)} />
-        <FormFooter isReply={isReplyModal} />
-      </div>
+      {insideForm}
     </form>
   );
 }
-
