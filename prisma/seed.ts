6da3ae540034b635f38a0ef23fe3503f36daa26a
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-17 11:48:20 +0700

    feat: update seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,53 +4,61 @@ const prisma = new PrismaClient();
 async function main() {
   await prisma.post.deleteMany()
   await prisma.user.deleteMany()
-  await prisma.user.createMany({
-    data: [
-      {
-        email: "hehe@gmail.com",
-        username: "user",
-        addname: "user",
-        birthdate: new Date("1990-01-01T00:00:00Z"),
-      },
-      {
-        email: "heho@gmail.com",
-        username: "user2",
-        addname: "user2",
-        birthdate: new Date("1990-01-01T00:00:00Z"),
-      },
-    ],
-  });
+  const usersData = [
+    {
+      email: "hehe@gmail.com",
+      name: "user",
+      addname: "user",
+      birthdate: new Date("1990-01-01T00:00:00Z"),
+    },
+    {
+      email: "heho@gmail.com",
+      name: "user2",
+      addname: "user2",
+      birthdate: new Date("1990-01-01T00:00:00Z"),
+    },
+  ];
+  
+  const users = [];
+  
+  for (const userData of usersData) {
+    const createdUser = await prisma.user.create({
+      data: userData,
+    });
+    users.push(createdUser);
+  }
+
   await prisma.post.createMany({
     data: [
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id: users[0].id,
       },
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id:users[0].id,
       },
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id:users[0].id,
       },
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id: users[1].id,
       },
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id:users[1].id,
       },
       {
         content:
           "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ex dignissimos asperiores sapiente facilis quidem molestiae natus illo ratione placeat. Laudantium repudiandae, voluptate natus cum nemo vitae perspiciatis? Pariatur, eius!",
-        author_id: 1,
+        author_id: users[1].id,
       },
     ],
   });
