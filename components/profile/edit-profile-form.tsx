da2911d489c6062dcccfe99595a261ff4d200e13
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-17 18:15:46 +0700

    feat: add validation to edit profile form

--- components/profile/edit-profile-form.tsx
+++ components/profile/edit-profile-form.tsx
@@ -2,6 +2,7 @@
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
@@ -12,27 +13,67 @@ import { Button } from "../ui/button";
 import { Textarea } from "../ui/textarea";
 import { Label } from "../ui/label";
 import { State, updateUserProfile } from "@/lib/actions/crud-user";
-import React from "react";
+import React, { useState } from "react";
 import { useFormState } from "react-dom";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
 export function InputForm({
   defaultValue,
   htmlName,
+  errors = [],
+  setSubmitable,
 }: {
   defaultValue: string;
   htmlName: string;
+  errors?: string[];
+  setSubmitable: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
+  const label = htmlName.charAt(0).toUpperCase() + htmlName.slice(1);
+  const [value, setValue] = useState(defaultValue);
+  const cantEmpty = htmlName === "username" || htmlName === "addname";
+  const warnEmpty = cantEmpty && value.length == 0;
   return (
-    <div>
-      <Label htmlFor={htmlName} className="text-lg">
-        {htmlName}
+    <div className="my-5">
+      <Label
+        htmlFor={htmlName}
+        className={`text-lg text-gray-500 ${warnEmpty && "text-red-500"} `}
+      >
+        {label}
       </Label>
-      <Textarea
-        defaultValue={defaultValue}
-        id={htmlName}
-        name={htmlName}
-        className="text-lg"
-      />
+      <div className="relative">
+        <Textarea
+          value={value}
+          id={htmlName}
+          name={htmlName}
+          className={`text-lg ${
+            htmlName === "bio" ? "h-[100px]" : "h-[50px]"
+          } ${
+            warnEmpty &&
+            "ring-red-500 ring-2 focus-visible:ring-2 focus-visible:ring-red-500"
+          }`}
+          spellCheck={false}
+          onChange={(e) => {
+            setValue(e.target.value);
+            if(cantEmpty){
+              setSubmitable(
+                e.target.value.length > 0
+              );
+            }
+          }}
+        />
+        <p className={`absolute right-1 top-0 ${warnEmpty && "text-red-500"}`}>
+          {value.length}/50
+        </p>
+      </div>
+      {warnEmpty && (
+        <p className="mt-2 text-sm text-red-500">{label} can&apos;t be empty</p>
+      )}
+      {errors.map((error: string) => (
+        <p className="mt-2 text-sm text-red-500" key={error}>
+          {error}
+        </p>
+      ))}
     </div>
   );
 }
@@ -54,7 +95,8 @@ export default function EditProfileForm({
   const initialState: State = { message: null, errors: {} };
   const updateUserWithId = updateUserProfile.bind(null, profileUser.id);
   const [state, formAction] = useFormState(updateUserWithId, initialState);
-  console.log(state);
+  const [submitable, setSubmitable] = useState(true);
+  console.log(submitable);
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -63,70 +105,57 @@ export default function EditProfileForm({
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[600px]">
-        <DialogHeader>
-          <DialogTitle className="text-2xl">Edit profile</DialogTitle>
-        </DialogHeader>
-        <form action={formAction}>
+        <form action={formAction} className="relative">
+          <DialogHeader className="mb-5 flex items-center">
+            <DialogPrimitive.Close className="rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-neutral-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-neutral-100 data-[state=open]:text-neutral-500 dark:ring-offset-neutral-950 dark:focus:ring-neutral-300 dark:data-[state=open]:bg-neutral-800 dark:data-[state=open]:text-neutral-400">
+              <Cross2Icon className="h-6 w-6" />
+              <span className="sr-only">Close</span>
+            </DialogPrimitive.Close>
+            <DialogTitle className="text-2xl ml-10">Edit profile</DialogTitle>
+            <DialogClose className="ml-auto">
+              <Button
+                type="submit"
+                className="text-lg rounded-full font-extrabold border-2 border-foreground px-5"
+                disabled={!submitable}
+              >
+                Save
+              </Button>
+            </DialogClose>
+          </DialogHeader>
           <InputForm
             defaultValue={profileUser.username ?? ""}
             htmlName="username"
+            errors={state.errors?.username}
+            setSubmitable={setSubmitable}
           />
-          {/* <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.username &&
-              state.errors.username.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div> */}
           <InputForm
             defaultValue={profileUser.addname ?? ""}
             htmlName="addname"
+            errors={state.errors?.addname}
+            setSubmitable={setSubmitable}
           />
-          {/* <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.addname &&
-              state.errors.addname.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div> */}
-          <InputForm defaultValue={profileUser.bio ?? ""} htmlName="bio" />
-          {/* <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.bio &&
-              state.errors.bio.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div> */}
+          <InputForm
+            defaultValue={profileUser.bio ?? ""}
+            htmlName="bio"
+            errors={state.errors?.bio}
+            setSubmitable={setSubmitable}
+          />
           <InputForm
             defaultValue={profileUser.location ?? ""}
             htmlName="location"
+            errors={state.errors?.location}
+            setSubmitable={setSubmitable}
           />
-          {/* <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.location &&
-              state.errors.location.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div> */}
           <InputForm
             defaultValue={profileUser.website ?? ""}
             htmlName="website"
+            errors={state.errors?.website}
+            setSubmitable={setSubmitable}
           />
-          {/* <div id="customer-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.website &&
-              state.errors.website.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div> */}
-          <Button type="submit">Save</Button>
         </form>
-        <DialogFooter></DialogFooter>
+        <DialogFooter>
+          <DialogClose></DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
