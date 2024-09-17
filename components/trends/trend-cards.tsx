786b3d490628e94fe53396cb1415946349b3f8a8
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-17 13:05:27 +0700

    refactor prettier

--- components/trends/trend-cards.tsx
+++ components/trends/trend-cards.tsx
@@ -1,27 +1,39 @@
-'use client';
+"use client";
 
 import { cn } from "@/lib/utils";
 import { TrendCard } from "./trend-card";
 import Link from "next/link";
 import Search from "../parts/search-input";
 import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 
-export function TrendCards({ className,sideTrend }: { className?: string, sideTrend:boolean }) {
+export function TrendCards({
+  className,
+  sideTrend,
+}: {
+  className?: string;
+  sideTrend: boolean;
+}) {
   const pathname = usePathname();
   return (
     <>
-      {!(sideTrend && pathname==="/explore") && <><Search placeholder="Search"></Search>
-      <div className={cn("border", "mt-3", "p-3", " rounded-xl", className)}>
-        <h1 className="text-xl font-bold">Trends For You</h1>
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
-        <TrendCard />
-        <Link className="text-blue-400" href={"/explore"}>
-          Show more
-        </Link>
-      </div>
-      </>}
+      {!(sideTrend && pathname === "/explore") && (
+        <Suspense>
+          <Search placeholder="Search"></Search>
+          <div
+            className={cn("border", "mt-3", "p-3", " rounded-xl", className)}
+          >
+            <h1 className="text-xl font-bold">Trends For You</h1>
+            <TrendCard />
+            <TrendCard />
+            <TrendCard />
+            <TrendCard />
+            <Link className="text-blue-400" href={"/explore"}>
+              Show more
+            </Link>
+          </div>
+        </Suspense>
+      )}
     </>
   );
 }
