22f03c450ead24e6fb21f17af1b560a9949f9a7b
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-20 23:08:15 +0700

    feat: remove side follow cards

--- app/(content)/layout.tsx
+++ app/(content)/layout.tsx
@@ -1,4 +1,3 @@
-import { FollowCards } from "@/components/follow-rec/followRec-card";
 import { SideNav } from "@/components/sidenav/side-nav";
 import { TrendCards } from "@/components/trends/trend-cards";
 
@@ -9,7 +8,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="w-[700px] border-x">{children}</div>
       <div className="px-4 lg:w-[400px] lg:items-start hidden lg:block sticky top-0 h-screen">
         <TrendCards sideTrend={true} />
-        <FollowCards />
       </div>
     </div>
   );
