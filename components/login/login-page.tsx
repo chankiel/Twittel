7ac68793c94458c472dd03246eb78d4e88727616
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-21 14:22:24 +0700

    feat: add styling to login page

--- components/login/login-page.tsx
+++ components/login/login-page.tsx
@@ -1,22 +1,25 @@
 import { TwitterLogoIcon } from "@radix-ui/react-icons";
 import { signIn } from "@/lib/actions/auth";
-
+import { Github } from "lucide-react";
 import { Button } from "../ui/button";
 
 export default function LoginPage() {
   return (
     <>
       <div className="flex justify-center gap-5 h-screen flex-col sm:flex-row items-start sm:items-center">
-        <TwitterLogoIcon className="m-5 sm:w-[500px] sm:h-[500px] w-[100px] h-[100px]" />
-        <div className="h-[500px] p-5">
-          <h1 className="font-bold text-4xl sm:text-7xl sm:mb-16 mb-5">Happening Now</h1>
+        <TwitterLogoIcon className="mx-auto sm:m-5 sm:w-[500px] sm:h-[500px] w-[100px] h-[100px]" />
+        <div className="h-[500px] p-5 sm:mx-0 mx-auto">
+          <h1 className="font-bold text-4xl sm:text-7xl sm:mb-16 mb-5">
+            Happening Now
+          </h1>
           <h2 className="font-bold text-2xl sm:text-5xl">Join today.</h2>
-          <form action={signIn} className="w-1/2">
+          <form action={signIn} className="sm:w-1/2 w-full">
             <Button
               type="submit"
-              className="rounded-full py-6 w-full mt-6 sm:text-lg font-bold"
+              className="rounded-full py-6 w-full mt-6 sm:text-lg font-bold flex items-center gap-2"
             >
-              Sign in With Github
+              <Github />
+              <p>Sign in With Github</p>
             </Button>
           </form>
         </div>
