0fdda572e6d05225b6587de95716778ee1e739d3
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-23 22:48:51 +0700

    feat: add createUser for register

--- lib/actions/crud-user.ts
+++ lib/actions/crud-user.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import prisma from "../db";
 import { z } from "zod";
 import paths from "@/path";
+import bcrypt from "bcrypt";
 
 export async function toggleFollow(
   userId1: string,
@@ -52,6 +53,8 @@ export type State = {
     bio?: string[];
     location?: string[];
     website?: string[];
+    email?: string[];
+    password?: string[];
   };
   message?: string | null;
 };
@@ -59,21 +62,38 @@ export type State = {
 const UserSchema = z.object({
   username: z.string().min(1,{message: 'Username can\'t be empty.'}),
   addname: z.string().min(1,{message: "Addname can\'t be empty."}),
-  bio: z.string(),
-  location: z.string(),
-  website: z.string(),
+  email: z.string().email({message: "Email invalid!"}),
+  bio: z.string().optional(),
+  location: z.string().optional(),
+  website: z.string().optional(),
+  password: z.string().superRefine((value, ctx) => {
+    const errors = [];
+  
+    if (value.length < 8) {
+      errors.push("at least 8 characters long");
+    }
+    if (!/[a-z]/.test(value)) {
+      errors.push("at least 1 lowercase letter");
+    }
+    if (!/[A-Z]/.test(value)) {
+      errors.push("at least 1 uppercase letter");
+    }
+    if (!/\d/.test(value)) {
+      errors.push("at least 1 number");
+    }
+    if (!/[\W_]/.test(value)) {
+      errors.push("at least 1 special character");
+    }
+  
+    if (errors.length > 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `Password must contain ${errors.join(", ")}.`,
+      });
+    }
+  })
+  
 });
-
-async function checkExistedUsername(username: string, userId: string) {
-  const user = await prisma.user.findFirst({
-    where: {
-      username,
-      NOT: { id: userId },
-    },
-  });
-  return !!user;
-}
-
 
 async function checkExistedAddname(addname: string, userId: string) {
   const user = await prisma.user.findFirst({
@@ -85,8 +105,17 @@ async function checkExistedAddname(addname: string, userId: string) {
   return !!user;
 }
 
-export async function updateUserProfile(
-  userId: string,
+async function checkExistedEmail(email: string, userId: string) {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+      NOT: { id: userId },
+    },
+  });
+  return !!user;
+}
+
+export async function createUserProfile(
   prevState: State,
   formData: FormData
 ): Promise<State> {
@@ -96,8 +125,69 @@ export async function updateUserProfile(
     bio: formData.get("bio"),
     location: formData.get("location"),
     website: formData.get("website"),
+    password: formData.get('password'),
+    email: formData.get('email'),
   });
-  console.log(validatedFields);
+  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const { username, addname, bio, location, website, password, email } = validatedFields.data;
+  if (await checkExistedAddname(addname,"")) {
+    return {
+      errors: {
+        addname: ["A user with that addname already exists."],
+      },
+    };
+  }
+  if (await checkExistedEmail(email,"")) {
+    return {
+      errors: {
+        email: ["A user with that email already exists."],
+      },
+    };
+  }
+  
+  const hashedPassword = await bcrypt.hash(password.toString(),10);
+  try {
+    await prisma.user.create({
+      data: {
+        username,
+        addname,
+        bio,
+        location,
+        website,
+        hashedPassword,
+        email,
+      },
+    });
+  } catch (error) {
+    console.error("Prisma create error:", error);
+  }
+
+  return {
+    message: addname,
+    errors: {},
+  };
+}
+
+const UpdateSchema = UserSchema.omit({password:true,email:true});
+
+export async function updateUserProfile(
+  userId: string,
+  prevState: State,
+  formData: FormData
+): Promise<State> {
+  const validatedFields = UpdateSchema.safeParse({
+    username: formData.get("username"),
+    addname: formData.get("addname"),
+    bio: formData.get("bio"),
+    location: formData.get("location"),
+    website: formData.get("website"),
+  });
   
   if (!validatedFields.success) {
     return {
@@ -106,14 +196,6 @@ export async function updateUserProfile(
   }
 
   const { username, addname, bio, location, website } = validatedFields.data;
-
-  if (await checkExistedUsername(username,userId)) {
-    return {
-      errors: {
-        username: ["A user with that username already exists."],
-      },
-    };
-  }
 
   if (await checkExistedAddname(addname,userId)) {
     return {
