f9cd71b6ee7ed54c925ba04d78ddae6e125d6719
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-23 22:50:31 +0700

    refactor: change to signInGithub

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,11 +1,16 @@
-'use server';
+"use server";
 
 import * as auth from "@/auth";
 
-export async function signIn(){
-    return auth.signIn('github');
-  }
-  
-  export async function signOut(){
-    return auth.signOut();
-  }
+export async function signInGithub() {
+  return auth.signIn("github");
+}
+
+export async function signOut() {
+  return auth.signOut();
+}
+
+// export async function signIn(formData: FormData){
+//   const provider = formData.get("provider") as string;
+//   return auth.signIn(provider);
+// }
