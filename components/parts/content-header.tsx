4d57546583c2bab0944ace26d28d4bcb85f376dc
Ignatius Jhon Hezkiel Chan <13522029@std.stei.itb.ac.id>
2024-09-23 22:47:30 +0700

    feat: add header for general

--- components/parts/content-header.tsx
+++ components/parts/content-header.tsx
@@ -1,21 +1,23 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
+import React from "react";
 
-interface ProfileHeaderProps {
-  username: string;
-  postsCount: number;
+interface ContentHeaderProps {
+  children: React.ReactNode;
+  className?: string;
 }
 
-export default function ProfileHeader({
-  username,
-  postsCount,
-}: ProfileHeaderProps) {
+export default function ContentHeader({
+  children,
+  className,
+}: ContentHeaderProps) {
   const router = useRouter();
 
   return (
-    <div className="flex items-center px-3 gap-7 py-1">
+    <div className={cn("flex items-center px-3 gap-7 py-1", className)}>
       <button
         className="h-6"
         onClick={() => {
@@ -25,8 +27,7 @@ export default function ProfileHeader({
         <ArrowLeftIcon className="h-full" />
       </button>
       <div>
-        <h1 className="text-2xl font-bold leading-none">{username}</h1>
-        <p>{postsCount} posts</p>
+        {children}
       </div>
     </div>
   );
